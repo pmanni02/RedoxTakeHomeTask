097df2ebc01cdeb72f571bbc5d884b8f60db34b8
PM <pm@PMs-MacBook-Pro.hsd1.wa.comcast.net>
2022-04-27 17:32:05 -0700

    Initial commit

---
+++ cli.js
@@ -0,0 +1,102 @@
+#!/usr/bin/env node
+
+const R = require('ramda');
+const cli = require('commander');
+const moment = require('moment');
+const { fetchPRs, getPRs, clearPRs } = require('./index');
+
+const isValidDateInput = (dates) => {
+    // initialize to true - any validation failures will stop checks
+    let isValid = true;
+    if(dates && dates.length == 2){
+        dates.forEach(date => {            
+            // moment(date to check, format, strict parsing)
+            const formatCheck = moment(date, 'M/D/YYYY', true);
+            const validFormat = formatCheck.isValid();
+
+            if(!validFormat){ isValid = false }
+        });
+    } else {
+        isValid = false;
+    }
+    return isValid;
+}
+
+const validDateTimestampCombo = (state, timestampType) => {
+    // valid combinations of state and timestamp type
+    // eg: an invalid combo is state=open, timestamp type = closed bc there will be no 
+    //      closed timestamp on open PRs
+    const validCombos = {
+        'open': ['created', 'updated'],
+        'closed': ['created', 'closed'],
+        'merged': ['created', 'closed'],
+        'all': ['created', 'updated', 'closed']
+    }
+    let validTypes = validCombos[state];
+    return validTypes.includes(timestampType);
+}
+
+cli
+    .description('Ramda Org Pull Request Console App')
+    .addHelpText('after', `
+Example calls:
+    $ github-pr-cli fetch                                             - fetch PRs from GitHub
+    $ github-pr-cli get all                                           - get number of all fetched PRs
+    $ github-pr-cli get -s merged                                     - get number of all PRs with 'merged' state
+    $ github-pr-cli get -s closed -l                                  - list PRs with 'closed' state
+    $ github-pr-cli get -s closed -t created -d 3/1/2020 5/20/2022 -l - list PRs with 'closed' state that were created between 3/1/2020 5/20/2022
+    $ github-pr-cli get -t closed -d 1/2/2015 1/3/2022 -l -s merged   - list PRs with 'merged' state that were closed between 1/2/2015 1/3/2022`)
+
+cli
+    .command('fetch')
+    .description('fetch ALL PRs from Ramda Org via GitHub v3 API')
+    .action(()=> {
+        fetchPRs()
+    });
+
+cli
+    .command('clear')
+    .description('clear data stored in memory')
+    .action(()=> {
+        clearPRs()
+    });
+
+cli
+    .command('get')
+    .description('lookup data')
+    .option('-s, --state <state>', 'filter by current state ("open", "closed", "merged") - omit to get ALL', "all")
+    .option('-l, --list', 'list the PRs', false)
+    .option('-d, --date <date...>', "filter by date range - valid format: 'M/D/YYY'")
+    .option(
+        '-t, --timestamp-type <type>', 
+        `filter date range by timestamp type ("created", "updated", "closed") - MUST be used with -date option`
+    )
+    .action((options) => {
+        // console.log(options)
+        // console.log(cli.args)
+        const { state, timestampType, date } = options;
+        
+        const validTypes = ["created", "updated", "closed"];
+        const validStates = ["open", "closed", "merged", "all"];
+        const validDateAndTime = (!date && timestampType) ? false : true;
+
+        // validate user input
+        if(!validStates.includes(state)){
+            console.log('Invalid state');
+            console.log('Valid states - open, closed, merged');
+        } else if(!validDateAndTime){
+            console.log('Timestamp type must be used with date flag - see examples')
+        } else if(date && !isValidDateInput(date)){
+            console.log('Invalid date(s) - see examples');
+        } else if(timestampType && !validTypes.includes(timestampType)){
+            console.log('Invalid timestamp type');
+            console.log('Valid types - created, updated, closed')
+        } else if (state && timestampType && !validDateTimestampCombo(state, timestampType)){
+            console.log('Invalid state and timestamp type combination - see examples');
+        } else {
+            // fetch date with valid input
+            getPRs(options);
+        }
+    });
+
+cli.parse(process.argv);
---
+++ index.js
@@ -0,0 +1,187 @@
+const axios = require('axios');
+const R = require('ramda');
+const parse = require('parse-link-header');
+
+const loki = require('lokijs');
+let db = new loki('db.json', { env: 'NODEJS' });
+let prs = db.addCollection("prs", { autoupdate: true });
+require('dotenv').config()
+
+// Base options for Github v3 Search API
+const baseURL = 'https://api.github.com';
+const baseOpts = {
+    url: `${baseURL}/search/issues?q=is:pr+org:ramda`,
+    method: 'get',
+    headers: {
+        "Authorization": `token ${process.env.API_KEY}`,
+        "Accept":"application/vnd.github.v3+json"
+    },
+    params: {
+        per_page: 100, //max 100, default 30
+    }
+}
+
+// Top level properties to pull from returned Pull Request object
+const topLevelProps = ['title', 'id', 'state', 'created_at', 'updated_at', 'closed_at', 'pull_request', 'user']
+
+// Minimum properties to display to user
+const minDisplayProps = ["title", "user", "created_at", "closed_at", "state"]
+
+// Get total pages per query
+async function getPages(queryModifier){
+    let numPages = 1;
+    try {
+        let opts = Object.assign({}, baseOpts);
+        opts.url = R.concat(opts.url, queryModifier);
+        const res = await axios(opts)
+
+        // parse link info from header object
+        const parsed = parse(res.headers.link);
+        if(parsed){
+            numPages = parsed.last.page;
+        }
+    } catch (error) {
+        console.log(error)
+    }
+    return numPages
+}
+
+// Transform timestamps into Date objects
+const transformTimestamps = (data) => {
+    const enUSFormat = new Intl.DateTimeFormat('en-US'); // M/D/YYYY
+    const timestamps = ["created_at", "updated_at", "merged_at", "closed_at"];
+
+    // creates an object with transformed dates for object merge with PR obj
+    // adds timestamps as Date Objects and as epochTime for comparisons
+    let dateObj = {};
+    R.forEach(timestamp => {
+        // save null instead of 'null' Date object val -> 1970-01-01T00:00:00.000Z
+        const date = data[timestamp] ? new Date(data[timestamp]) : null;
+        const formattedDate = date ? enUSFormat.format(date) : null;
+        const epochTime = date ? date.valueOf() : null;
+        dateObj[timestamp] = formattedDate;
+        dateObj[`${timestamp}_e`] = epochTime;
+    },timestamps);
+    return R.mergeLeft(dateObj, data);
+}
+
+// Display data
+//  data (obj)
+//  showData (boolean) -> if true, list data
+const display = (data, showData) => {
+    if(showData){
+        const minOutput = R.map(R.pick(minDisplayProps), data)
+        console.table(minOutput)
+    } 
+    console.log(`Total: ${data.length}`);
+}
+
+// Fetch data from GitHub, transform, and save
+async function requestData(queryModifier){
+    // call to fetch link headers
+    const pages = await getPages(queryModifier);
+    let opts = Object.assign({}, baseOpts);
+    opts.url = R.concat(opts.url, queryModifier);
+    // console.log(opts)
+    // make calls to get pages
+    for(let i=1; i<=pages; i++){
+        opts.params.page = i;
+        try {            
+            const res = await axios(opts);
+            const filter = (pr) => {
+                // pick out selected top level props
+                const strippedData = R.pick(topLevelProps, pr);
+
+                // move user name to base level
+                const dataWithUser = R.assoc('user', strippedData.user.login, strippedData);
+
+                // create merged status -> has a merged_at timestamp
+                const mergedTime = strippedData['pull_request']['merged_at'];
+                const dataWithMergedTS = R.assoc('merged_at', mergedTime, dataWithUser);
+
+                // convert timestamps to Date object
+                const fixedTimeStamp = transformTimestamps(dataWithMergedTS);
+                
+                // if the PR was merged, create a new state 'merged'. overwrites 'closed' state
+                if(mergedTime){
+                    const data = R.assoc('state', 'merged', fixedTimeStamp);
+                    prs.insert(data);
+                }else{
+                    prs.insert(fixedTimeStamp);
+                }
+            }
+
+            // for each returned PR obj, apply filters / transform data for storage
+            R.forEach(filter, res.data.items);
+            total = res.data.total_count;
+            db.saveDatabase();
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+// Fetch data - breakup requests for data by creation timestamp
+// Due to github request limits for the search api (1000 results per search), the requests
+//  are split by PR creation date. Each request returns < 1000 results
+const fetchPRs = async () => {
+    const firstPRYear = 2013; // fetch via api - will not change for ramda org
+    const currentYear = parseInt(new Date().getFullYear());
+    
+    let queryStrs = []
+    let year = firstPRYear;
+    while(year <= currentYear - 1){
+        let queryStr = `+created:${year}-01-01..${year+1}-12-31`;
+        queryStrs = R.append(queryStr, queryStrs)
+        year+=2
+    }
+
+    R.forEach(async (queryStr) => {
+        console.log('requesting data...')
+        await requestData(queryStr);
+    }, queryStrs);
+}
+
+// Get data
+//  options (obj) -> user input args and flags
+const getPRs = async (options) => {
+    const { state, list, date, timestampType } = options;
+    db.loadDatabase({}, function(){
+        let collection = db.getCollection('prs');
+        let data;
+        if(collection){
+            // retrieve initial dataset
+            if(state === "all"){
+                data = collection.chain();
+            } else {
+                data = collection.chain().find({ state });
+            }
+
+            // filter dataset by dates and timestampType
+            if(date){
+                const epoch = R.map(date => new Date(date).valueOf(), date);
+                const key = `${timestampType}_at_e`;
+                data = data.find({ [key]: { '$between': epoch } }); // [key] => string interpolation for obj key
+            }
+            
+            data = data.data({removeMeta: true})
+            display(data, list);
+        }else{
+            console.log('No data');
+        }
+    });
+}
+
+const clearPRs = async () => {
+    db.loadDatabase({}, function(){
+        db.getCollection("prs").clear({ removeIndices: true });
+        db.saveDatabase();
+    });
+}
+
+module.exports = {
+    fetchPRs,
+    getPRs,
+    clearPRs
+}
